66a0752215198bcd289395938948d83ffac019a4
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-04-25 13:13:52 +0530

    feat(express): app configuration complete

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import express, {
+  NextFunction,
+  Request,
+  Response,
+  ErrorRequestHandler,
+} from "express";
+import morgan from "morgan";
+import cors from "cors";
+import env from "./utils/env";
+import chat from "./api/chat";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: env.PAYLOAD_LIMIT,
+    strict: true,
+  })
+);
+
+app.use(
+  express.urlencoded({
+    limit: env.PAYLOAD_LIMIT,
+    extended: true,
+  })
+);
+
+if (env.isDev) {
+  app.use(morgan("dev"));
+} else {
+  app.use(morgan("tiny"));
+}
+
+app.use("/api/chat", chat);
+
+app.all("*path", (_req: Request, res: Response) => {
+  res.status(200).json({ message: "Chat AI - Powered By OpenAI & GetStream" });
+});
+
+app.use(((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(`Error: ${err.message}`);
+  res.status(500).json({ message: "Internal Server Error!" });
+}) as ErrorRequestHandler);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Chat AI Api");
+import mongodb from "./mongodb";
+import env from "./utils/env";
+import app from "./app";
+
+const uri = env.MONGODB_URI;
+const port = env.PORT;
+
+(async () => {
+  try {
+    const state = await mongodb(uri);
+    if (state === 1) {
+      console.log("Database connection success!");
+      app.listen(port, () => {
+        console.log(`Server running on port: ${port}\n`);
+      });
+    } else {
+      throw new Error("Database connection error!");
+    }
+  } catch (error: any) {
+    console.error(`Error: ${error.message}\n`);
+    process.exit(1);
+  }
+})();
