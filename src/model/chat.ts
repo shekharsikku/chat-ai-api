8f7ac36b9570718cc0a362b18be5f338c8cf400a
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-04-25 13:28:23 +0530

    feat(model): mongodb model for user and chat

---
+++ src/model/chat.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+
+const ChatSchema = new Schema(
+  {
+    uid: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    reply: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Chat = model("Chat", ChatSchema);
+
+export default Chat;
